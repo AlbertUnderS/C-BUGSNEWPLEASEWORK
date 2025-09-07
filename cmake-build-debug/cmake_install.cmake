a7bf24dfcfc43d690901dbdc6db5581fe202f053
Albert_Undferscore <aragon19alberta@gmail.com>
2025-09-07 21:36:27 +0100

    Created classes

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/arago/CLionProjects/CA3_CPP_AA
+# Install script for directory: C:/Users/arago/CLionProjects/CA3_CPPREPEAT_A_A_
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/arago/CLionProjects/CA3_CPP_AA/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/arago/CLionProjects/CA3_CPPREPEAT_A_A_/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
